4ef8858b47ba11000d850f54af9cf3886ebc281d
Justin Renjilian <justin.renjilian@gmail.com>
2020-01-08 20:33:56 -0500

    Whole bunch of terrible code to support note editing and deletion

--- src/Api.elm
+++ src/Api.elm
@@ -1,7 +1,8 @@
-module Api exposing (Note, getNotes)
+module Api exposing (Note, getNotes, saveNote, deleteNote)
 
 import Http
 import Json.Decode exposing (Decoder, field)
+import Json.Encode
 
 
 type alias Note =
@@ -16,9 +17,27 @@ type alias Note =
 getNotes : (Result Http.Error (List Note) -> msg) -> Cmd msg
 getNotes m =
     Http.get
-        { url = "https://swift-notes-api.herokuapp.com/api/notes"
+        { url = "http://localhost:8080/api/notes"
         , expect = Http.expectJson m (Json.Decode.list noteDecoder)
         }
+
+
+saveNote : (Result Http.Error Note -> msg) -> Note -> Cmd msg
+saveNote m note =
+    put
+        { url = "http://localhost:8080/api/notes/" ++ String.fromInt note.id
+        , body = Http.jsonBody (noteEncoder note)
+        , expect = Http.expectJson m noteDecoder
+        }
+
+
+deleteNote : (Result Http.Error () -> msg) -> Int -> Cmd msg
+deleteNote m id =
+    delete
+        { url = "http://localhost:8080/api/notes/" ++ String.fromInt id
+        , expect = Http.expectWhatever m
+        }
+    
 
 
 noteDecoder : Decoder Note
@@ -29,3 +48,40 @@ noteDecoder =
         (field "presenter" Json.Decode.string)
         (field "rating" Json.Decode.int)
         (field "notes" Json.Decode.string)
+
+
+noteEncoder : Note -> Json.Encode.Value
+noteEncoder note =
+    Json.Encode.object
+        [ ( "id", Json.Encode.int note.id )
+        , ( "title", Json.Encode.string note.title )
+        , ( "presenter", Json.Encode.string note.presenter )
+        , ( "rating", Json.Encode.int note.rating )
+        , ( "notes", Json.Encode.string note.note )
+        ]
+
+
+put : { url : String, body : Http.Body, expect : Http.Expect msg } -> Cmd msg
+put request =
+    Http.request
+        { method = "PUT"
+        , headers = []
+        , url = request.url
+        , body = request.body
+        , expect = request.expect
+        , timeout = Nothing
+        , tracker = Nothing
+        }
+
+
+delete : { url : String, expect : Http.Expect msg } -> Cmd msg
+delete request =
+    Http.request
+        { method = "DELETE"
+        , headers = []
+        , url = request.url
+        , body = Http.emptyBody
+        , expect = request.expect
+        , timeout = Nothing
+        , tracker = Nothing
+        }
--- src/Page/Home.elm
+++ src/Page/Home.elm
@@ -1,14 +1,22 @@
 module Page.Home exposing (Model, Msg, init, update, view)
 
 import Api exposing (Note)
+import Css exposing (..)
+import Dict exposing (Dict)
 import Html.Styled
     exposing
         ( Html
+        , button
         , div
+        , h1
         , header
-        , nav
+        , span
         , text
+        , form
+        , textarea
         )
+import Html.Styled.Attributes exposing (css, type_)
+import Html.Styled.Events as Events
 import Http
 import Page
 
@@ -18,21 +26,35 @@ import Page
 
 
 type alias Model =
-    { notes : List Note
-    , state : State
+    { notes : Dict Int Note
+    , requestState : RequestState
+    , editState : EditState
+    , confirm : ConfirmState
     }
 
 
-type State
+type RequestState
     = Loading
     | Success
     | Failure String
 
 
+type EditState
+    = NotEditing
+    | Editing Note
+
+
+type ConfirmState
+    = Hidden
+    | Active Int
+
+
 init : ( Model, Cmd Msg )
 init =
-    ( { notes = []
-      , state = Loading
+    ( { notes = Dict.empty
+      , requestState = Loading
+      , editState = NotEditing
+      , confirm = Hidden
       }
     , Api.getNotes ReceivedNotes
     )
@@ -44,18 +66,68 @@ init =
 
 type Msg
     = ReceivedNotes (Result Http.Error (List Note))
+    | StartEditing Note
+    | Save
+    | StopEditing
+    | SavedNote (Result Http.Error Note)
+    | OnNoteChange Note
+    | ShowDeleteConfirm Int
+    | Delete Int
+    | NoteDeleted Int (Result Http.Error ())
 
 
-update : Msg -> Model -> ( Model, Cmd msg )
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         ReceivedNotes result ->
             case result of
                 Ok notes ->
-                    ( { model | notes = notes, state = Success }, Cmd.none )
+                    ( { model
+                        | notes = List.foldl (\n -> Dict.insert n.id n) Dict.empty notes
+                        , requestState = Success
+                      }
+                    , Cmd.none
+                    )
 
                 Err _ ->
-                    ( { model | state = Failure "Something went wrong" }, Cmd.none )
+                    ( { model | requestState = Failure "Something went wrong" }, Cmd.none )
+
+        StartEditing note ->
+            ( { model | editState = Editing note }, Cmd.none )
+
+        OnNoteChange note ->
+            ( { model | editState = Editing note }, Cmd.none )
+
+        StopEditing ->
+            ( { model | editState = NotEditing }, Cmd.none )
+
+        Save ->
+            case model.editState of
+                NotEditing ->
+                    ( model, Cmd.none )
+                Editing note ->
+                    ( { model | editState = NotEditing }, Api.saveNote SavedNote note )
+
+        SavedNote result ->
+            case result of
+                Ok note ->
+                    ( { model | notes = Dict.insert note.id note model.notes }, Cmd.none )
+
+                _ ->
+                    ( model, Cmd.none )
+
+        ShowDeleteConfirm id ->
+            ( { model | confirm = Active id }, Cmd.none )
+
+        Delete id ->
+            ( model, Api.deleteNote (NoteDeleted id) id )
+
+        NoteDeleted id result ->
+            case result of
+                Ok _ ->
+                    ( { model | notes = Dict.remove id model.notes }, Cmd.none )
+                Err _ ->
+                    ( model, Cmd.none )
 
 
 
@@ -76,20 +148,33 @@ view model =
 viewHeader : Html Msg
 viewHeader =
     header
-        []
-        [ nav
+        [ css
+            [ padding (px 20)
+            , borderBottom3 (px 2) solid (hex "AAA")
+            ]
+        ]
+        [ div
             []
-            []
+            [ h1
+                [ css
+                    [ margin zero
+                    ]
+                ]
+                [ text "CodeMash Notes" ]
+            ]
         ]
 
 
 viewContent : Model -> Html Msg
 viewContent model =
     div
-        []
-        [ case model.state of
+        [ css
+            [ margin (px 20)
+            ]
+        ]
+        [ case model.requestState of
             Success ->
-                viewNotes model.notes
+                viewNotes model
 
             Failure err ->
                 text err
@@ -99,16 +184,223 @@ viewContent model =
         ]
 
 
-viewNotes : List Note -> Html Msg
-viewNotes notes =
+viewNotes : Model -> Html Msg
+viewNotes model =
     div
         []
-        (List.map viewNoteSummary notes)
+        (model.notes
+            |> Dict.map (\_ -> viewNoteSummary model)
+            |> Dict.values
+        )
 
 
-viewNoteSummary : Note -> Html Msg
-viewNoteSummary note =
+viewNoteSummary : Model -> Note -> Html Msg
+viewNoteSummary model note =
+    case model.editState of
+        Editing n ->
+            if n.id == note.id then
+                viewNoteEdit n
+
+            else
+                viewNoteDefault note
+
+        _ ->
+            viewNoteDefault note
+
+
+viewNoteDefault : Note -> Html Msg
+viewNoteDefault note =
     div
-        []
-        [ text note.title
+        [ css
+            [ padding (px 15)
+            , border3 (px 2) solid (hex "AAA")
+            , borderRadius (px 10)
+            , marginBottom (px 20)
+            ]
+        ]
+        [ div
+            [ css
+                [ marginBottom (px 10)
+                , paddingBottom (px 5)
+                , borderBottom3 (px 1) solid (hex "CCC")
+                ]
+            ]
+            [ span
+                [ css
+                    [ fontWeight bold
+                    ]
+                ]
+                [ text note.title ]
+            , span
+                [ css
+                    [ whiteSpace pre
+                    ]
+                ]
+                [ text " • " ]
+            , span
+                [ css
+                    [ color (hex "555")
+                    ]
+                ]
+                [ text note.presenter ]
+            , span
+                [ css
+                    [ whiteSpace pre
+                    ]
+                ]
+                [ text " • " ]
+            , span
+                [ css
+                    [ color
+                        (if note.rating > 7 then
+                            hex "50b946"
+
+                         else if note.rating > 5 then
+                            hex "fca403"
+
+                         else
+                            hex "ff0000"
+                        )
+                    ]
+                ]
+                [ text (String.fromInt note.rating ++ "/10") ]
+            , div
+                [ css
+                    [ float right
+                    , displayFlex
+                    , color (hex "555")
+                    , fontSize small
+                    ]
+                ]
+                [ button
+                    [ css
+                        [ margin zero
+                        ]
+                    , Events.onClick (StartEditing note)
+                    ]
+                    [ text "edit" ]
+                , div
+                    [ css
+                        [ whiteSpace pre
+                        ]
+                    ]
+                    [ text " | " ]
+                , button
+                    [ css
+                        [ margin zero
+                        ]
+                    , Events.onClick (Delete note.id)
+                    ]
+                    [ text "delete" ]
+                ]
+            ]
+        , div
+            []
+            [ text note.note
+            ]
+        ]
+
+
+viewNoteEdit : Note -> Html Msg
+viewNoteEdit note =
+    form
+        [ css
+            [ padding (px 15)
+            , border3 (px 2) solid (hex "AAA")
+            , borderRadius (px 10)
+            , marginBottom (px 20)
+            ]
+        , Events.onSubmit Save
+        ]
+        [ div
+            [ css
+                [ marginBottom (px 10)
+                , paddingBottom (px 5)
+                , borderBottom3 (px 1) solid (hex "CCC")
+                ]
+            ]
+            [ span
+                [ css
+                    [ fontWeight bold
+                    ]
+                ]
+                [ text note.title ]
+            , span
+                [ css
+                    [ whiteSpace pre
+                    ]
+                ]
+                [ text " • " ]
+            , span
+                [ css
+                    [ color (hex "555")
+                    ]
+                ]
+                [ text note.presenter ]
+            , span
+                [ css
+                    [ whiteSpace pre
+                    ]
+                ]
+                [ text " • " ]
+            , span
+                [ css
+                    [ color
+                        (if note.rating > 7 then
+                            hex "50b946"
+
+                         else if note.rating > 5 then
+                            hex "fca403"
+
+                         else
+                            hex "ff0000"
+                        )
+                    ]
+                ]
+                [ text (String.fromInt note.rating ++ "/10") ]
+            , div
+                [ css
+                    [ float right
+                    , displayFlex
+                    , color (hex "555")
+                    , fontSize small
+                    ]
+                ]
+                [ button
+                    [ css
+                        [ margin zero
+                        ]
+                    , type_ "submit"
+                    ]
+                    [ text "save" ]
+                , div
+                    [ css
+                        [ whiteSpace pre
+                        ]
+                    ]
+                    [ text " | " ]
+                , button
+                    [ css
+                        [ margin zero
+                        ]
+                    , Events.onClick StopEditing
+                    , type_ "button"
+                    ]
+                    [ text "cancel" ]
+                ]
+            ]
+        , textarea
+            [ css
+                [ width (pct 100)
+                , padding (px 5)
+                , margin zero
+                , boxSizing borderBox
+                , border3 (px 1) solid (hex "AAA")
+                , borderRadius (px 5)
+                , fontSize inherit
+                ]
+            , Events.onInput (\s -> OnNoteChange { note | note = s })
+            ]
+            [ text note.note
+            ]
         ]
